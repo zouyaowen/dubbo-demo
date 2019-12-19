90fa91bbde3c9e07e2a83b5c41ad17a36eafd50e
zouyaowen <1227701903@qq.com>
2019-12-20 00:20:59 +0800

    1220

---
+++ hello/src/main/java/com/zou/demo/HelloApplication.java
@@ -0,0 +1,17 @@
+package com.zou.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: zouyaowen
+ * @Description:
+ * @Date: 0:54 2019/11/28
+ * @Modifyed by:
+ */
+@SpringBootApplication
+public class HelloApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApplication.class, args);
+    }
+}
